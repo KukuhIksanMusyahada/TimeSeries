c10223eda1930ed91e41a1caf6cec187659c41fb
Kukuh Iksan Musyahada <kukuh.iksan@gmail.com>
2022-06-26 23:03:24 +0700

    add data_process function

--- StockPrediction/LSTM/DataProcessing/dataprocess.py
+++ StockPrediction/LSTM/DataProcessing/dataprocess.py
@@ -8,10 +8,18 @@ from LSTM.Essential import path_handling as ph
 from LSTM.Essential import global_params as gp
 
 
-def retrive_data(names= 'AAPL',path= ph.GetDataSource()):
+def retrieve_data(names= 'AAPL',path= ph.GetDataSource()):
     data = yf.download(names,
         start= gp.START_DATE,
         end= gp.END_DATE,
         progress= False)
-    pd.to_csv(os.path.join(path, names, '.csv'))
+    data.to_csv(os.path.join(path, names, '.csv'))
     return data
+
+def data_process(data= retrieve_data()):
+    data['Date'] =  data.index
+    data = data[["Date", "Open", "High", "Low", "Close", 
+             "Adj Close", "Volume"]]
+    data.reset_index(drop=True, inplace= True)
+    return data
+    
